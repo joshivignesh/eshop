73650d67e4ceea925fdd374e79f31689991f7b1c
Vignesh <joshivignesh@users.noreply.github.com>
2023-10-23 18:13:16 +0530

    Added Initial header items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className='header__logo'>
+        </div>
+      <div className='header__search'>
+        </div>
+      <div className='header__nav'>
+        <div className='nav__item'>
+          <span  class="nav__itemLineOne">Hello Guest</span> 
+          <span  class="nav__itemLineTwo">Sign In</span> 
+          </div>  
+        <div className='nav__item'>
+          <span  class="nav__itemLineOne">Your</span> 
+          <span  class="nav__itemLineTwo">Shop</span> 
+          </div>  
+        <div className='nav__item'>
+            <ShoppingBasketIcon fontSize="large" />
+            <span  class="nav__itemLineTwo">0</span> 
+          </div>  
+        </div>
     </div>
   );
 }
