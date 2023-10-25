27ef5ba34cf1b590fde12d54214123fa833fe691
Vignesh <joshivignesh@users.noreply.github.com>
2023-10-25 23:07:20 +0530

    Changed header

--- src/Header.js
+++ src/Header.js
@@ -27,6 +27,10 @@ function Header() {
             <ShoppingBasketIcon fontSize="large" />
             <span  class="nav__itemLineTwo">0</span> 
           </div>  
+          <div className='nav__item'>
+            <ShoppingBasketIcon fontSize="large" />
+            <span  class="nav__itemLineTwo">0</span> 
+          </div> 
         </div>
 
 
